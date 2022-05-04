08ccd950b05f6cf6397b21361ff0c652c56e338e
yuki_sugawara <eimoxshsf@gmail.com>
2022-05-04 09:12:50 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Restaurant Search"
+  end
+
 end

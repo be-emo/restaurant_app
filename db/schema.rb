300250755c993d206621250ce767f22cee2f39c9
yuki_sugawara <eimoxshsf@gmail.com>
2022-05-11 06:40:53 +0000

    Modify README/static_pages_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_05_11_040932) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "open"
+    t.string "photo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
